2354e609988530168178aa0181336186c387b07e
unknown <gongyusi@game.ntes>
2020-07-24 17:48:20 +0800

    添加mybatis和mybatis-generator

--- src/main/java/com/example/mall/Application.java
+++ src/main/java/com/example/mall/Application.java
@@ -1,9 +1,12 @@
 package com.example.mall;
 
+//import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.mall.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/example/mall/controller/indexController.java
+++ src/main/java/com/example/mall/controller/indexController.java
@@ -1,15 +1,24 @@
 package com.example.mall.controller;
 
+import com.example.mall.dao.UserInfoMapper;
+import com.example.mall.entity.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class indexController {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+
     @RequestMapping(path={"/index","/"})
-    @ResponseBody
-    public String index(){
-        return "Hello World";
+    public List<UserInfo> selectTest(){
+        return userInfoMapper.selectAll();
     }
 }
